5fa80afe49f6ce279859461f8d6f8d57e9906027
abc <abc@gmail.com>
2023-07-28 10:20:40 +0700

    xóa logit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,52 +14,19 @@ let users = [
 ];
 
 // Trả về trang HTML hiển thị danh sách users
-const searchUsers = (req, res) => {
-  res.render('pages/users/index', {
-    title: 'Danh sách người dùng',
-    users: users,
-  });
-};
+const searchUsers = (req, res) => {};
 
 // Trả về HTML - form thêm mới user
-const viewAddUser = (req, res) => {
-  res.render('pages/users/new');
-};
+const viewAddUser = (req, res) => {};
 
 // Thực thi add user: nhận request từ form thêm mới user
-const addUser = (req, res) => {
-  const input = req.body;
-
-  const newUser = {
-    ...input,
-    id: getNextId(users),
-  };
-
-  users.push(newUser);
-
-  // Chuyển hướng về trang danh sách
-  res.redirect('/users');
-};
+const addUser = (req, res) => {};
 
 // Trả về HTML - thông tin user
 const getDetailUser = (req, res) => {};
 
 // Trả về HTML form cập nhật user
-const viewEditUser = (req, res) => {
-  const { id } = req.params;
-
-  const user = users.find((user) => user.id == id);
-
-  if (user) {
-    res.render('pages/users/edit', {
-      user: user,
-    });
-  } else {
-    res.render('errors/404', {
-      msg: 'Người dùng không tồn tại',
-    });
-  }
-};
+const viewEditUser = (req, res) => {};
 
 // Thực thi cập nhật user
 const updateUser = (req, res) => {
@@ -67,39 +34,14 @@ const updateUser = (req, res) => {
 
   const input = req.body;
 
-  users = users.map((user) => {
-    if (user.id == id) {
-      return {
-        ...user,
-        username: input.username,
-        email: input.email,
-      };
-    } else {
-      return user;
-    }
-  });
+  users = users.map((user) => {});
 
   // Chuyển hướng về trang danh sách
   res.redirect('/users');
 };
 
 // Thực thi xóa user
-const deleteUser = (req, res) => {
-  const { id } = req.params;
-
-  const user = users.find((user) => user.id == id);
-
-  if (user) {
-    users = users.filter((user) => user.id != id);
-
-    // Chuyển hướng về trang danh sách
-    res.redirect('/users');
-  } else {
-    res.render('errors/404', {
-      msg: 'Người dùng không tồn tại',
-    });
-  }
-};
+const deleteUser = (req, res) => {};
 
 export default {
   searchUsers,
