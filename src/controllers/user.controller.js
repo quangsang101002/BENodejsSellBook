f4f6b4d48400410cd9fb8f3c17795faa65c99834
abc <abc@gmail.com>
2023-07-28 15:16:59 +0700

    .

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -14,34 +14,57 @@ let users = [
 ];
 
 // Trả về trang HTML hiển thị danh sách users
-const searchUsers = (req, res) => {};
+const searchUsers = (req, res) => {
+  res.render('pages/users/index', {
+    title: 'welcome',
+    users: users,
+  });
+};
 
 // Trả về HTML - form thêm mới user
-const viewAddUser = (req, res) => {};
+const viewAddUser = (req, res) => {
+  res.render('pages/users/new');
+};
 
 // Thực thi add user: nhận request từ form thêm mới user
-const addUser = (req, res) => {};
+const addUser = (req, res) => {
+  users.push({
+    ...req.body,
+    id: getNextId(users),
+  });
+  res.redirect('/users');
+};
 
 // Trả về HTML - thông tin user
-const getDetailUser = (req, res) => {};
+const getDetailUser = (req, res) => {
+  const { id } = req.params;
+
+  const search = users.filter((user) => user.id == id);
+
+  res.render('pages/users/index', {
+    title: 'welcome',
+    users: search,
+  });
+};
 
 // Trả về HTML form cập nhật user
 const viewEditUser = (req, res) => {};
 
 // Thực thi cập nhật user
-const updateUser = (req, res) => {
-  const { id } = req.params;
-
-  const input = req.body;
-
-  users = users.map((user) => {});
-
-  // Chuyển hướng về trang danh sách
-  res.redirect('/users');
-};
+const updateUser = (req, res) => {};
 
 // Thực thi xóa user
-const deleteUser = (req, res) => {};
+const deleteUser = (req, res) => {
+  const { id } = req.params;
+
+  const deleteuser = users.filter((user) => user.id != id);
+
+  res.render('pages/users/index', {
+    title: 'welcome',
+    users: deleteuser,
+  });
+  res.redirect('/users');
+};
 
 export default {
   searchUsers,
